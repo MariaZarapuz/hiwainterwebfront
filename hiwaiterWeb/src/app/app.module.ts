0be81a2b84ea4aac8e0629d972efd355fee4756b
Maria Perez Zarapuz <mariazarapuz@gmail.com>
2020-07-06 13:11:03 +0200

    create components principal

--- hiwaiterWeb/src/app/app.module.ts
+++ hiwaiterWeb/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from '../app/components/home/home.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { HeaderComponent } from './components/shared/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
