7e53a6b0d13b9fff4185709ab03f9cf139184616
Maria Perez Zarapuz <mariazarapuz@gmail.com>
2020-07-07 13:24:56 +0200

    layout finish login

--- hiwaiterWeb/src/app/app.module.ts
+++ hiwaiterWeb/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {ReactiveFormsModule} from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +9,7 @@ import { HomeComponent } from '../app/components/home/home.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
 import { HeaderComponent } from './components/shared/header/header.component';
 import { ModalLoginComponent } from './components/modal-login/modal-login.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
@@ -18,7 +21,8 @@ import { ModalLoginComponent } from './components/modal-login/modal-login.compon
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- hiwaiterWeb/src/app/components/modal-login/modal-login.component.ts
+++ hiwaiterWeb/src/app/components/modal-login/modal-login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-modal-login',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./modal-login.component.css']
 })
 export class ModalLoginComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor() {
+    this.form = new FormGroup({
+      cif: new FormControl(''),
+      password: new FormControl('')
+
+      });
+   }
 
   ngOnInit(): void {
   }
+  onSubmit() { console.log(this.form.value); }
 
 }
